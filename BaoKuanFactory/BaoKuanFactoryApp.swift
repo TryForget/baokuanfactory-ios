f444e4ab0589aca33d0b0a8ee5f78874f46d1187
TryForget <70316795+TryForget@users.noreply.github.com>
2026-04-16 15:28:02 +0800

    Initial Commit

---
+++ BaoKuanFactory/BaoKuanFactoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaoKuanFactoryApp.swift
+//  BaoKuanFactory
+//
+//  Created by 李大帅 on 2026/4/16.
+//
+
+import SwiftUI
+
+@main
+struct BaoKuanFactoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
